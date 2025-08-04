5719e3244b1046eb8c04214406098f0d6d822c6f
alxtim10 <alxtim10@gmail.com>
2025-08-04 20:48:25 +0700

    add sidebar and dashboard

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,14 @@
+import { DashboardSidebar } from "@/components/DashboardSidebar"
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <SidebarProvider>
+      <DashboardSidebar />
+      <main>
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  )
+}
